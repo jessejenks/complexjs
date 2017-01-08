209b70a319eb0f0146bb30a269cc6400a0f32db4
Sam Burdick <smburdick@pugetsound.edu>
2017-01-07 21:50:28 -0800

    gamma function, edited tostring

--- complex.js
+++ complex.js
@@ -1,16 +1,42 @@
+/**
+ * A library of complex variables and functions, emphasizing Riemann Zeta
+ * @author Jesse E Jenks
+ * @author Sam Burdick
+ */
+
+// constants
+var EPSILON = 0.0000001;
+var PI = new Complex(Math.PI,0);
+
+// a complex variable
+
 function Complex(real, imag) {
   this.real = real;
   this.imag = imag;
-  this.toString = function() {
-    if (this.imag>=0) return this.real+' + '+this.imag+'i';
-    else return this.real+''+this.imag+'i';
+  this.toString = function() { // TODO: decide how to print if re or im == 0
+    var op;
+    var im = this.imag;
+    if (this.imag >= 0){
+      op = '+';
+    } else {
+      op = '-';
+    }
+    if(this.imag === 1 || this.imag === -1) {
+      im = '';
+    }
+    return this.real + ' ' + op + ' ' + im + 'i';
   }
 };
+
+// Elementary operations
+
 /*
+* Examples:
 * var w = new Complex(2,5); // 2+5i
 * var z = new Complex(1,1); // 1+2i
 * var w_x_z = w.multiply(z); // (2*1 - 5*2) + (2*2 + 5*1)i
 */
+
 Complex.prototype.multiply = function(z) {
   return new Complex(this.real*z.real - this.imag*z.imag, this.real*z.imag + this.imag*z.real);
 };
@@ -27,7 +53,7 @@ Complex.prototype.power = function(n) {
 Complex.prototype.add = function(z) {
   return new Complex(this.real+z.real, this.imag+z.imag);
 };
-Complex.prototype.addReal = function(r){
+Complex.prototype.add_real = function(r){
  return new Complex(this.real+ r, this.imag);
 };
 Complex.prototype.subtract = function(z) {
@@ -48,8 +74,38 @@ Complex.prototype.raise_to = function (c) {
   return new Complex(coeff*Math.cos(theta),coeff*Math.sin(theta));
 };
 Complex.prototype.e_to_the = function() {
-  return new Complex(exp(this.real)*cos(this.imag),exp(this.real)*sin(this.imag));
+  return new Complex(Math.exp(this.real)*Math.cos(this.imag),Math.exp(this.real)*Math.sin(this.imag));
 };
+Complex.prototype.distance_sqr = function(z){
+  return (this.real-z.real)*(this.real-z.real)+(this.imag-z.imag)*(this.imag-z.imag);
+};
+Complex.prototype.magnitude = function() {
+  return sqrt(this.real*this.real + this.imag*this.imag);
+};
+Complex.prototype.mag_sqr = function(){
+  return this.real*this.real + this.imag*this.imag;
+};
+Complex.prototype.sub_real = function(x) {
+  return new Complex(this.real - x, this.imag);
+};
+Complex.prototype.add_real = function(x) {
+  return new Complex(this.real + x, this.imag);
+};
+Complex.prototype.raise_to_z = function(z) {
+  var a = this.real;
+  var b = this.imag;
+  var c = z.real;
+  var d = z.imag;
+  var arg = this.arg();
+  var asbs = (a*a + b*b);
+  var param = c*arg + (1/2) * d * Math.log(asbs);
+  var to_return = new Complex ( Math.cos(param), Math.sin(param));
+  var sc = Math.pow(asbs , c / 2) * Math.pow(Math.E , -d*arg);
+  return to_return.scalarMult(sc);
+};
+
+// Taylor evaluations of trig fns
+
 Complex.prototype.sin_taylor = function() {
 // Complex.prototype.sin_taylor = function(k) {
   // if (k === 1) return this;
@@ -59,19 +115,58 @@ Complex.prototype.sin_taylor = function() {
   return this.subtract(this.power(3).scalarMult(1/6)).add(this.power(5).scalarMult(1/120)).subtract(this.power(7).scalarMult(1/5040)).add(this.power(9).scalarMult(1/362880));
 };
 Complex.prototype.cos_taylor = function() {
-  // z.power(2).multiply().addReal(1);
-  return this.power(2).scalarMult(-1/2).add(this.power(4).scalarMult(1/24)).subtract(this.power(6).scalarMult(1/720)).add(this.power(8).scalarMult(1/40320)).addReal(1);
+  // z.power(2).multiply().add_real(1);
+  return this.power(2).scalarMult(-1/2).add(this.power(4).scalarMult(1/24)).subtract(this.power(6).scalarMult(1/720)).add(this.power(8).scalarMult(1/40320)).add_real(1);
 };
 
-Complex.prototype.gamma = function() {
-  if (this.imag === 0) {
-    // and this.real is a positive integer...
-    if (this.real === 1) return this;
-    else return new Complex(this.real - 1, 0).gamma().multiply(this);
+// Gamma function
+
+// polynomial coeffs
+var P = [ 676.5203681218851,   -1259.1392167224028,
+          771.32342877765313, -176.61502916214059,
+          12.507343278686905, -0.13857109526572012,
+          9.9843695780195716e-6, 1.5056327351493116e-7];
+
+/**
+ * Lanczos approximation of Γ(z) implementation based on https://en.wikipedia.org/wiki/Lanczos_approximation
+ * @param Complex z
+ */
+function gamma(z) {
+  var result;
+  var h = 0.5;
+  if (z.real < h) {
+    result = PI.divide( z.scalarMult(Math.PI)
+                         .sin_taylor()
+                         .multiply( gamma( z.scalarMult(-1).add_real(1) ) ) );
   } else {
-
+    z.real -= 1;
+    var x = new Complex(0.99999999999980993 , 0);
+    var d = new Complex(1,1);
+    for(var i = 0; i < P.length; i++) {
+      x = x.add( new Complex(P[i] , 0)
+            .divide( z.add_real(i+1) ) );
+    }
+    var t = z.add_real(P.length)
+             .sub_real(h);
+    result = x.scalarMult( Math.sqrt(2 * Math.PI) )
+              .multiply( t.raise_to_z( z.add_real(h) ) )
+              .multiply( t.scalarMult(-1).e_to_the() );
   }
-};
+  if (within_epsilon(result.imag)) {
+    return result.real;
+  }
+  return result;
+}
+
+/**
+ * @param real x
+ */
+function within_epsilon(x) {
+  return Math.abs(x) <= EPSILON;
+}
+
+// Riemann Zeta function
+
 Complex.prototype.zeta_n = function () {
   // d/ds zeta_N = - sum_{n=1}^{N} log(n)/n^s
 };
@@ -84,7 +179,7 @@ Complex.prototype.zeta = function(epsilon) {
   if (this.real === 0 && this.imag === 0) return new Complex(-1/2, 0);
   if (this.real === 1 && this.imag === 0) return new Complex(Infinity, 0);
   if (this.real < 0) {
-    var news = this.scalarMult(-1).addReal(1);
+    var news = this.scalarMult(-1).add_real(1);
     return news.scalarMult(-1).raise_to(2*Math.PI)
     .multiply(this.scalarMult(Math.PI/2).sin_taylor())
     .multiply(news.gamma())
@@ -99,13 +194,4 @@ Complex.prototype.zeta = function(epsilon) {
   //
   //   }
   // }
-};
-Complex.prototype.distance_sqr = function(z){
-  return (this.real-z.real)*(this.real-z.real)+(this.imag-z.imag)*(this.imag-z.imag);
-};
-Complex.prototype.magnitude = function() {
-  return sqrt(this.real*this.real + this.imag*this.imag);
-};
-Complex.prototype.mag_sqr = function(){
-  return this.real*this.real + this.imag*this.imag;
 };
